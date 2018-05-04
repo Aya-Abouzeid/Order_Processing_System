38514ea9d98fe32317227fc8327b774bbd290768
aya aly abouzeid <aya_aly_abouzeid@yahoo.com>
2018-05-05 01:35:47 +0200

    Add Amira's GUIs Version#2

--- src/GUI/AddToCartGUI.java
+++ src/GUI/AddToCartGUI.java
@@ -8,32 +8,24 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class CartGUI {
+public class AddToCartGUI {
 	private RadioButton ISBN = new RadioButton("ISBN");
 	private TextField ISBNTf = new TextField();
 	private RadioButton Quantity = new RadioButton("Quantity");
 	private TextField QuantityTf = new TextField();
 	private Button AddToCart = new Button();
-	private Button View = new Button();
-	private Button Remove = new Button();
-	private Button Price = new Button();
-	private Button TotalPrice = new Button();
-
-
-
 
 	private Button Back = new Button();
 	private Scene CustomerScene;
 	private Stage stage;
 
-	public CartGUI( Stage primaryStage, Scene s) {
+	public AddToCartGUI( Stage primaryStage, Scene s) {
 		stage = primaryStage;
 		CustomerScene = s;
 		CartPage();
@@ -67,45 +59,32 @@ public class CartGUI {
 		});
  }
 	public void FillGUI(GridPane gp){
-		AddToCart.setText("Add Books To Cart");
-		AddToCart.setPrefSize(300, 35);
-		gp.add(AddToCart, 1, 0);
-		AddToCart.setStyle("-fx-background-color: #006064; -fx-text-fill: white; -fx-font: normal bold 25px 'serif' ;");
-		
-		View.setText("View Books In Cart");
-		View.setPrefSize(300, 35);
-		gp.add(View, 1, 1);
-		View.setStyle("-fx-background-color: #006064; -fx-text-fill: white; -fx-font: normal bold 25px 'serif' ;");
-		
-		TotalPrice.setText("View Total Price");
-		TotalPrice.setPrefSize(300, 35);
-		gp.add(TotalPrice, 1, 2);
-		TotalPrice.setStyle("-fx-background-color: #006064; -fx-text-fill: white; -fx-font: normal bold 25px 'serif' ;");
 		
 		ISBN.setStyle("-fx-font: normal bold 32px 'serif' ");
-		gp.add(ISBN, 1, 3);
+		gp.add(ISBN, 1, 2);
+		
+		Quantity.setStyle("-fx-font: normal bold 32px 'serif' ");
+		gp.add(Quantity, 1, 3);
 		
 		ISBNTf.setVisible(false);
-		gp.add(ISBNTf, 2, 3);
+		gp.add(ISBNTf, 3, 2);
 		
-		Price.setText("View Book Price");
-		Price.setPrefSize(300, 35);
-		gp.add(Price, 1, 4);
-		Price.setStyle("-fx-background-color: #006064; -fx-text-fill: white; -fx-font: normal bold 25px 'serif' ;");
+		QuantityTf.setVisible(false);
+		gp.add(QuantityTf, 3, 3);
 		
-		Remove.setText("Remove Book");
-		Remove.setPrefSize(300, 35);
-		gp.add(Remove, 1, 5);
-		Remove.setStyle("-fx-background-color: #006064; -fx-text-fill: white; -fx-font: normal bold 25px 'serif' ;");
-		
-		
+		AddToCart.setText("Add To Cart");
+		AddToCart.setPrefSize(200, 35);
+		gp.add(AddToCart, 1, 4);
+		AddToCart.setStyle("-fx-background-color: #006064; -fx-text-fill: white; -fx-font: normal bold 25px 'serif' ;");
+
 		
 		Back.setText("< Back");
 		Back.setPrefSize(119, 35);
-		gp.add(Back, 1, 6);
+		gp.add(Back, 1, 5);
 		Back.setStyle("-fx-background-color: #006064; -fx-text-fill: white; -fx-font: normal bold 25px 'serif' ;");
 
 	}
+	
 	
 public void RadioBtnFunctionality(){
 		
@@ -122,25 +101,30 @@ public void RadioBtnFunctionality(){
 	            }
 	        }
 	    });
-}
+		Quantity.setOnAction(new EventHandler<ActionEvent>() {
 
-public void AddFunctionality(){
-	
-	Back.setOnMouseClicked(new EventHandler<MouseEvent>() {
+	        @Override
+	        public void handle(ActionEvent arg0) {
+	            if(Quantity.isSelected()){
+	            	QuantityTf.setVisible(true);
+	            }
+	            else{
+	            	QuantityTf.clear();
+	            	QuantityTf.setVisible(false);
+	            }
+	        }
+	    });
 
-		@Override
-		public void handle(MouseEvent arg0) {
-			stage.setScene(CustomerScene);
-			stage.show();
-		}
-	});
-	AddToCart.setOnMouseClicked(new EventHandler<MouseEvent>() {
+	}
+	public void AddFunctionality(){
+		
+		Back.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
-		@Override
-		public void handle(MouseEvent arg0) {
-			AddToCartGUI add = new AddToCartGUI(stage , CustomerScene);
-		}
-	});
-	
-}
+			@Override
+			public void handle(MouseEvent arg0) {
+				stage.setScene(CustomerScene);
+				stage.show();
+			}
+		});
+	}
 }
