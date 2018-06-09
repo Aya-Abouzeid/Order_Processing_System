4a83d27fb6d8c528799c0a0a31ff8094ea7d4a8e
Aya-Abouzeid <eng-aya.abouzaid1419@alexu.edu.eg>
2018-06-09 14:24:28 +0200

    Main.cpp Added

--- main.cpp
+++ main.cpp
@@ -42,28 +42,56 @@ int random_threshold() {
     os << digit;
     return stoi(os.str());
 }
+int random_isbn(){
+    ostringstream os;
+    int digit = (rand() % 1000) + 1;
+    os << digit;
+
+    cout<< stoi(os.str());
+}
+int random_author() {
+    ostringstream os;
+
+    int digit = rand() % 6;
+    os << digit;
+    return stoi(os.str());
+}
+
 
 int main(int argc, const char *argv[]) {
 
     string categories[] = {"Science", "Art", "Religion", "History", "Geography"};
 
+    string authors [] = {"M.Ahmed","hassan hussein","ahmed hamdy","eman shawky","Hala Moustafa ","Youssef A.Darwish"};
+    freopen("./author.txt", "w", stdout);
 
-
-    freopen("./BOOK.txt", "w", stdout);
-    int j = 1980;
-    for (int i = 1; i <= 5000; ++i, j++) {
+    for (int i = 1; i <= 100; ++i) {
         cout << "aaa"
              << to_string(i)
              << ","
-             << "\"" << "BOOK" << to_string(i) << "\""
+             << authors[random_author()]
+             //             << ","
+             //             << "\"" << std::setfill('a') << std::setw(6) << to_string(i) << "\""
+             << endl;
+
+    }
+
+    freopen("./BOOK.txt", "w", stdout);
+    int j = 1980;
+
+    for (int i = 1; i <= 200000; ++i, j++) {
+        cout << "aaa"
+             << to_string(i+800000)
              << ","
-             << to_string((i % 10) +1)   // pid
+             << "BOOK" << to_string(i+800000)
+             << ","
+             << to_string((i % 6) +1)   // pid
              << ", "
-              << to_string(j) <<"-01-01"        //year
+             << to_string(j) <<"-01-01"        //year
              << ","
              << random_price()      //price
              << " ,"
-             << "\"" << categories[random_category()] << "\""   //category
+             << categories[random_category()]   //category
              << ","
              << random_stock()    //stock
              << ","
@@ -76,5 +104,30 @@ int main(int argc, const char *argv[]) {
     }
 
 
+    freopen("./Author.txt", "w", stdout);
+
+    // start with : i=1,i+=2
+    // start with : i=2,i+=2
+    // then random to make random books have more than 1 author
+    // i+=10
+    //i+=100
+    //i+=50
+
+
+
+    for (int i = 2,j=1; i <= 1000000; i+=4,j++) {
+        cout << "aaa"
+             << to_string(i)
+             << ","
+             << "Author" << to_string(j)
+             << endl;
+
+        if(j>500000)
+            j=1;
+    }
+
+
+
     return 0;
 }
+
